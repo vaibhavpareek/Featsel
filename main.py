1436e04bc52b740d343ac65d7f149ee3a4d75206
Vaibhav Pareek <vaibhavpareekvp7@gmail.com>
2020-04-17 19:52:30 +0530

    Project new modified

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pandas import read_csv,get_dummies,DataFrame,Series
 import cv2
 import sys
 from seaborn import heatmap,pairplot
-from os import system
+from os import system,popen
 from sklearn.feature_selection import mutual_info_regression as mi
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -11,7 +11,7 @@ from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import Lasso
 from statsmodels.api import OLS
 from pyfiglet import Figlet
-from clint.textui import puts, colored, indent
+from clint.textui import colored
 
 def banner():
 	banner1 = Figlet(font='sblood')
@@ -24,6 +24,11 @@ def exit_banner():
 	banner1 = Figlet(font='digital')
 	print("\n")
 	print(colored.yellow(banner1.renderText("Thanks for Using FeatSel")))
+
+def version():
+	print("FeatSel 1.0.0")
+	exit_banner()
+	sys.exit(0)
 
 def filter(dataset):
 	print(dataset.corr())
@@ -155,13 +160,69 @@ def tui_interface(tui):
 			sys.exit(0)	
 		except FileNotFoundError:
 			print("DataSet Not Found")
+
+def help():
+	try:	
+		system("chmod +x manual")
+		system("man ./manual")
+		exit_banner(0)
+	except:
+		sys.exit(0)
+
+def setup():
+	try:
+		system("python3 setup.py")
+		print("Environment Setup Successfully")
+		exit_banner()
+		sys.exit(0)
+	except FileNotFoundError:
+		print("Setup file is missing")
+		sys.exit(0)
+
+def list_dataset():
+	try:
+		print("List of Dataset's Locally Present")
+		print("\033[1;33;48m")
+		ls = popen('ls Demodataset/').read()
+		if(len(ls)==0):
+			print("--no-local dataset")
+		else:
+			print(ls)
+		exit_banner()
+		sys.exit(0)
+	except Exception as e:
+		print(e)
+		print("Some Error Occured in Listing Dataset")
+		sys.exit()	
+
+def add_dataset():
+	print("\033[1;32;48m")
+	print("Add DataSet to Local DataBase")
+	df = input("Location of the DataSet : ")
+	df_name = df.split("/")
+	try:
+		system('ls Demodataset/ > temp.txt')
+		f = open("temp.txt","r")
+		if(str(df_name[-1]) in f):
+			print("DataSet with this name already exist.")
+		else:
+			system("cp "+df+" Demodataset/")
+			print("Successfully Added")
+		system('rm temp.txt')
+		exit_banner()
+		sys.exit(0)
+	except Exception as e:
+		print("Error Occured - Dataset Not Added")
+		sys.exit()	
+
 def main():
 	arg=list(sys.argv)
-	try :
-		if(len(arg) > 2):
-			print("More Arguments are passed then Required")
-			sys.exit(0)
-		else:
+	if(len(arg) > 2):
+		print("More Arguments are passed then Required")
+		exit_banner()
+		sys.exit(0)
+	else:
+		try:				
 			if(arg[1]=="--help" or arg[1]=="-h"):
 				help()
 			elif(arg[1]=="--tui" or arg[1]=="-t"):
@@ -199,21 +260,26 @@ def main():
 			elif(arg[1]=="--version"or arg[1]=="-v"):
 				print("\033[1;34;48m")
 				version()
+			elif(arg[1]=="--setup"or arg[1]=="-s"):
+				print("\033[1;34;48m")
+				setup()
+			elif(arg[1]=="--list" or arg[1]=="-l"):
+				print("\033[1;34;48m")
+				list_dataset()
+			elif(arg[1]=="--add" or arg[1]=="-a"):
+				print("\033[1;34;48m")
+				add_dataset()
 			else:
-				print(str(arg[1]+"not supported by FeatSel"))
+				print(str(arg[1]+" not supported by FeatSel"))
 				exit_banner()
 				sys.exit()
-
-	except FileNotFoundError:
-		print("DataSet Not Found")
-		exit_banner()
-		sys.exit(0)
-	except:
-		print("Some Error Occured")
-		exit_banner()
-		sys.exit(0)
-
-
+		except FileNotFoundError:
+			print("DataSet Not Found")
+			exit_banner()
+			sys.exit(0)
+		except Exception as e:
+			print("2."+str(e))
+			print("Some Error Occured")
+			exit_banner()
+			sys.exit(0)
 main()
-
-
---
+++ setup.py
@@ -0,0 +1,2 @@
+echo "alias FeatSel='python3 $PWD/main.py'" >> ~/.bashrc
+echo "alias demodataset='echo $PWD/Demodataset'" >> ~/.bashrc
